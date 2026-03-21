714ef3af29516aa8c234b6c49c4812d5dcc177d9
Damien <danglevt1234@gmail.com>
2026-03-21 20:40:12 +0700

    update login dags

--- versions/v2_distributed/pipelines/login_sites.py
+++ versions/v2_distributed/pipelines/login_sites.py
@@ -39,25 +39,38 @@ async def login_buff(browser, username:str, password:str):
     context = await browser.new_context(
         user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36"
     )
-    page = await context.new_page()
+    page = await browser.new_page()
+    await page.goto("https://buff.163.com",wait_until="domcontentloaded", timeout=120000)
 
-    await page.goto("http://buff.163.com", wait_until="domcontentloaded", timeout=60000)
+    # Wait for login button or perform manual login
+    await page.wait_for_selector("text=Login/Register", timeout=120000)
     await page.click("text=Login/Register")
-
+    # Prepare to catch the popup (Steam login)
     async with page.expect_popup() as popup_info:
+        await page.is_visible("text=Other login methods",timeout=120000)
         await page.click("text=Other login methods")
-        await page.wait_for_timeout(10000)
-
+        
+    #inputing account's info ( im using an freshly created account, without steam guard and such)
     popup = await popup_info.value
-    await popup.fill("input[type='text']", username)
-    await popup.fill("input[type='password']", password)
-    await popup.wait_for_timeout(10000)
 
-    await popup.wait_for_selector("button:has-text('Sign In')")
-    await popup.click("button:has-text('Sign In')")
-    await popup.wait_for_timeout(10000)
+    login_section = popup.locator("div.page_content").locator("div[data-featuretarget='login']")
+    username_input = login_section.locator("div", has_text="Sign in with account name").locator("input[type='text']")
+    await username_input.is_visible(timeout=30000)
+    password_input = login_section.locator("div", has_text="Password").locator("input[type='password']")
+    await password_input.is_visible(timeout=30000)
 
-    await popup.locator('input[value="Sign In"]').click()
+    # Fill credentials
+    await username_input.fill("username")
+    await password_input.fill("password")
+    await login_section.locator("button", has_text="Sign in").click()
+
+    # Click the sign-in button to login buff
+    final_login_form = popup.locator("form#openidForm")
+    sign_in_button = final_login_form.locator("input[type='submit']", has_text="Sign In")
+
+    # Wait for it to be visible and click
+    await sign_in_button.wait_for(state="visible", timeout=30000)
+    await sign_in_button.click()
 
     cookies = await page.context.cookies()
     target_domain = "buff.163.com"
